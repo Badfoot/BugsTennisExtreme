53e07af2f9a3d33323f15fd0fe57c693dfd93715
Badfoot <jmra18@gmail.com>
2015-06-06 20:10:26 +0200

    Nuevas Clases

--- js/main/Game.js
+++ js/main/Game.js
@@ -1,8 +1,11 @@
 var game = new Phaser.Game(500, 340, Phaser.AUTO, 'gameDiv');
+game.global = {
+	//
+};
 
-game.state.add('boot', bootState);
-game.state.add('load', loadState);
-game.state.add('menu', menuState);
-game.state.add('play', playState);
+game.state.add('Boot', bootState);
+game.state.add('Load', loadState);
+game.state.add('Menu', menuState);
+game.state.add('Play', playState);
 
-game.state.start('boot');
+game.state.start('Boot');
---
+++ js/scenes/Play.js
@@ -0,0 +1,20 @@
+var playState = {
+
+	create: function() { 
+		//
+	},
+
+	update: function() {
+		//
+	},
+
+	startMenu: function() {
+		//game.state.start('Menu');
+	},
+
+	createWorld: function() {
+ 		//
+	},
+
+	//
+};
